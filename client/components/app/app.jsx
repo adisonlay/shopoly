11513fbef02113771d85355655281fd486f359d8
Adison Lay <adisonlay@gmail.com>
2019-11-18 09:42:23 -0800

    Created basic skeleton of main app component

--- client/components/app/app.jsx
+++ client/components/app/app.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import Header from './header';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      view: {
+        name: 'catalog',
+        params: {}
+      },
+      activeCartID: null,
+      cartItems: [],
+      orderedCarts: []
+    };
+  }
+  setView() {
+
+  }
   render() {
     return (
       <div>
-
+        <Header />
       </div>
     );
   }
