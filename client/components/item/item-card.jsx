24d02f57918513c120156993c431abb2d8944177
Adison Lay <adisonlay@gmail.com>
2019-11-18 14:22:35 -0800

    Set up render for ItemCard component

--- client/components/item/item-card.jsx
+++ client/components/item/item-card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { sortImageData } from '../app/functions'
+import { Card, CardActionArea, CardActions, CardContent, CardMedia, Box, Typography, Button } from '@material-ui/core';
 
 export default function ItemCard({ itemData, setAppView }) {
   const {
@@ -13,10 +14,34 @@ export default function ItemCard({ itemData, setAppView }) {
   } = itemData;
 
   images = sortImageData(images);
+  const handleDetailsClick = () => props.setAppView('details', { itemID: id });
 
   return (
-    <div>
-
-    </div>
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          image={`../../${images[0]}`}
+          title={name}
+        />
+        <CardContent>
+          <Box display="flex" justifyContent="space-between">
+            <Typography gutterBottom variant="h5">
+              {name}
+            </Typography>
+            <Typography gutterBottom variant="h5" color="textSecondary">
+              {`$${price}`}
+            </Typography>
+          </Box>
+          <Typography>Lot Number: {lotNumber}</Typography>
+          <Typography>Base Rent: {`$${baseRent}`}</Typography>
+          <Typography>Color Group: {itemGroup}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" onClick={handleDetailsClick}>
+          View Product Details
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
