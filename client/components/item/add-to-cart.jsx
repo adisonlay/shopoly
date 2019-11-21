0a868ae632c10ad745f2214f43f7f0c26970a6aa
Adison Lay <adisonlay@gmail.com>
2019-11-21 03:34:59 -0800

    Added component for add to cart button on item details page

---
+++ client/components/item/add-to-cart.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { FormControl, InputLabel, Select, MenuItem, Button } from '@material-ui/core';
+
+export default function AddToCart({ addToCartCallback }) {
+  const [quantity, setQuantity] = useState(1);
+
+  useEffect(() => {}, []);
+
+  const handleSelect = () => {};
+
+  return (
+    <div>
+      <FormControl variant="outlined">
+        <InputLabel ref={inputLabel} id="quantity-select-label">
+          Age
+        </InputLabel>
+
+        <Select
+          labelId="quantity-select-label"
+          id="quantity-select"
+          value={quantity}
+          onChange={handleSelect}
+        >
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+          <MenuItem value={4}>4</MenuItem>
+        </Select>
+
+        <Button variant="contained" color="primary">Add to Cart</Button>
+      </FormControl>
+    </div>
+  );
+}
--- client/components/item/item-details.jsx
+++ client/components/item/item-details.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
+import AddToCart from './add-to-cart';
 import { formatItemData } from '../app/functions'
-import { Container, Grid, Paper, Box, Typography, Button } from '@material-ui/core';
+import { Container, Grid, Paper, Box, Typography } from '@material-ui/core';
 
 export default function ItemDetails({ setAppView, viewParams, addToCartCallback }) {
   const [itemDetailData, setItemDetailData] = useState({});
@@ -18,8 +19,6 @@ export default function ItemDetails({ setAppView, viewParams, addToCartCallback 
       abortController.abort();
     };
   }, [itemDetailData]);
-
-  const handleAddToCart = () => {};
 
   if (Object.keys(itemDetailData).length === 0) {
     return (<Typography variant="h5" color="textSecondary">Item details unavailable.</Typography>);
@@ -42,6 +41,7 @@ export default function ItemDetails({ setAppView, viewParams, addToCartCallback 
         <Paper>
           <Box p="2rem">
             <Grid container>
+
               <Grid item xs={12} md={6} lg={4}>
                 <Box
                   mb="1rem"
@@ -55,19 +55,24 @@ export default function ItemDetails({ setAppView, viewParams, addToCartCallback 
                   }}
                 />
               </Grid>
+
               <Grid item xs={12} md={6} lg={6}>
-                <Typography gutterBottom variant="h5">{name}</Typography>
-                <Typography gutterBottom variant="h6" color="textSecondary">{price}</Typography>
-                <Typography>Lot Number: {lotNumber}</Typography>
-                <Typography>Base Rent: {rent}</Typography>
-                <Typography gutterBottom>Color Group: {itemGroup}</Typography>
-                <Button variant="contained" color="primary">Add to Cart</Button>
+                <Box mb="1rem">
+                  <Typography gutterBottom variant="h5">{name}</Typography>
+                  <Typography gutterBottom variant="h6" color="textSecondary">{price}</Typography>
+                  <Typography>Lot Number: {lotNumber}</Typography>
+                  <Typography>Base Rent: {rent}</Typography>
+                  <Typography gutterBottom>Color Group: {itemGroup}</Typography>
+                  <AddToCart />
+                </Box>
               </Grid>
+
             </Grid>
+
             <Typography paragraph>{description1}</Typography>
             <Typography paragraph>{description2}</Typography>
-          </Box>
 
+          </Box>
         </Paper>
       </Container>
     );
