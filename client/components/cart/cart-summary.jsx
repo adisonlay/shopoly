7bc87d5185eeb1da5e57ce09880619e83c8dff1a
Adison Lay <adisonlay@gmail.com>
2019-11-21 16:00:42 -0800

    Set up basic layout for CartSummary page

--- client/components/cart/cart-summary.jsx
+++ client/components/cart/cart-summary.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import { formatItemData } from '../app/functions';
-import { Container, Grid, Paper, Box, Typography } from '@material-ui/core';
+import { Container, Grid, Paper, Box, Typography, Button } from '@material-ui/core';
 
 export default function CartSummary({ setAppView, cartItems }) {
-  return (
-    <div>
+  let cartItemListDisplay = null;
+  let checkoutButtonDisplay = null;
 
-    </div>
+  const handleCheckout = () => setAppView('checkout', { itemTotal: '' });
+
+  if (!cartItems.length) {
+    cartItemListDisplay = (<Typography variant="h5" color="textSecondary">There are no items in your cart.</Typography>);
+  } else {
+    cartItemListDisplay = (cartItems.map(cartItem => <CartSummaryItem key={cartItem.itemID} itemData={cartItem} />));
+    checkoutButtonDisplay = (<Button variant="contained" color="primary" onClick={handleCheckout}>Checkout</Button>);
+  }
+
+  return (
+    <Container fixed>
+      <Paper>
+        <Box p="2rem">
+          <Grid container>
+            <Grid item xs={12} md={6} lg={4}>
+
+            </Grid>
+          </Grid>
+        </Box>
+      </Paper>
+    </Container>
   );
 }
