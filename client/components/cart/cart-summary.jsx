8c984a131bae1da9a00c29c81ec8c01ba4a0be48
Adison Lay <adisonlay@gmail.com>
2019-11-26 04:54:39 -0800

    Refactored helper functions to use reduce array method to shorten code in CartSummary

--- client/components/cart/cart-summary.jsx
+++ client/components/cart/cart-summary.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import CartSummaryItem from './cart-summary-item';
-import { formatItemData } from '../app/functions';
 import { Container, Box, Typography, Button } from '@material-ui/core';
 
 export default function CartSummary({ setAppView, cartItems }) {
   let cartItemListDisplay = null;
   let checkoutButtonDisplay = null;
+
+  let initialCartItemCount = 0;
+  const cartItemCount = cartItems.reduce((runningCount, currentItemObject) => runningCount + currentItemObject.quantity, initialCartItemCount);
+
+  let initialCartTotal = 0;
+  const cartTotal = cartItems.reduce((runningTotal, currentItemObject) => runningTotal + currentItemObject.finalPrice, initialCartTotal);
 
   const handleCheckout = () => setAppView('checkout', { itemTotal: '' });
 
@@ -20,7 +25,7 @@ export default function CartSummary({ setAppView, cartItems }) {
     <Container fixed>
       {cartItemListDisplay}
       <Box display="flex" justifyContent="space-between">
-        <Typography>Cart Total: </Typography>
+        <Typography variant="h5">Cart Total ({cartItemCount} Items): {cartTotal}</Typography>
         {checkoutButtonDisplay}
       </Box>
     </Container>
