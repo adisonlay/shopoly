13181ed17db66b70fca2a6de3d2a91de80ff7012
Adison Lay <adisonlay@gmail.com>
2019-11-29 17:48:08 -0800

    Set up skeleton for CheckoutForm component

---
+++ client/components/cart/cart-checkout-form.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Grid, Box, Paper, Typography, List, ListItem, ListItemText, Divider } from '@material-ui/core';
+
+export default function CartCheckoutForm({ setAppView, viewParams, placeOrderCallback }) {
+  return (
+    <div>
+
+    </div>
+  );
+}
