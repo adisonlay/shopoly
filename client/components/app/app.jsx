10262a29ab9204eff00575f3805c8fe0ea713c0d
Adison Lay <adisonlay@gmail.com>
2019-12-01 15:16:47 -0800

    Added placeOrder method to App to be called after checking out from cart

--- client/components/app/app.jsx
+++ client/components/app/app.jsx
@@ -5,19 +5,17 @@ import ItemCardsList from '../item/item-cards-list';
 import ItemDetails from '../item/item-details';
 import CartSummary from '../cart/cart-summary';
 import CartCheckoutForm from '../cart/cart-checkout-form';
-import { Box } from '@material-ui/core';
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
       view: { page: 'catalog', params: {} },
-      activeCartID: null,
-      cartItems: [],
-      orderedCarts: []
+      cartItems: []
     };
     this.setView = this.setView.bind(this);
     this.addToCart = this.addToCart.bind(this);
+    this.placeOrder - this.placeOrder.bind(this);
   }
 
   setView(page, params) {
@@ -48,6 +46,21 @@ export default class App extends Component {
       .catch(error => console.error(error));
   }
 
+  getOrderHistory() { }
+
+  placeOrder(activeCartID) {
+    fetch('/api/order/order.php', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(activeCartID)
+    })
+      .then(() => this.setState({
+        view: { page: 'orderSummary', params: {} },
+        cartItems: []
+      }))
+      .catch(error => console.error(error));
+  }
+
   componentDidMount() {
     this.getCartItems();
   }
@@ -61,17 +74,17 @@ export default class App extends Component {
       catalog: (<ItemCardsList setAppView={this.setView} />),
       details: (<ItemDetails setAppView={this.setView} viewParams={currentParams} addToCartCallback={this.addToCart} />),
       cart: (<CartSummary setAppView={this.setView} cartItems={this.state.cartItems} />),
-      checkout: (<CartCheckoutForm setAppView={this.setView} viewParams={currentParams} cartItems={this.state.cartItems} />),
+      checkout: (<CartCheckoutForm setAppView={this.setView} viewParams={currentParams} cartItems={this.state.cartItems} placeOrderCallback={this.placeOrder} />),
       orderHistory: '',
       orderSummary: ''
     };
 
     return (
-      <Box>
+      <div>
         <Header setAppView={this.setView} cartItemCount={cartItemCount} />
         <BreadcrumbBar setAppView={this.setView} currentView={currentPage} itemName={currentPage === 'details' ? currentParams.itemName : null} />
         {pageComponents[currentPage]}
-      </Box>
+      </div>
     );
   }
 }
