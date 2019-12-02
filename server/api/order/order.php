8e1ac3da818d9e07d2ac6e59a72f85fd148b66b6
Adison Lay <adisonlay@gmail.com>
2019-12-01 16:40:13 -0800

    Added basic routing endpoint for orders in order.php

---
+++ server/api/order/order.php
@@ -0,0 +1,18 @@
+<?php
+
+define('INTERNAL', true);
+require_once('../helper/functions.php');
+session_start();
+set_exception_handler('handleErrors');
+require_once('../../dbconnection.php');
+
+switch ($_SERVER['REQUEST_METHOD']) {
+  case 'GET':
+    include('order-get.php');
+    break;
+  case 'POST':
+    include('order-add.php');
+    break;
+}
+
+?>
