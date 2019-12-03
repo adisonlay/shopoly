a5b797ebca19b52239381101bf8fd5ac6cdb868e
Adison Lay <adisonlay@gmail.com>
2019-12-03 14:30:02 -0800

    Wrote fetch call for OrderHistory component to get order history data from order endpoint

--- client/components/order/order-history.jsx
+++ client/components/order/order-history.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { formatItemData } from '../app/functions';
+import { Container, Grid, Paper, Box, Typography } from '@material-ui/core';
 
 export default function OrderHistory() {
-  return (
-    <div>
+  const [orderHistoryData, setOrderHistoryData] = useState([]);
 
-    </div>
-  );
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    fetch('api/order/order.php', { signal })
+      .then(response => response.json())
+      .then(orderHistoryData => setOrderHistoryData(orderHistoryData))
+      .catch(error => console.error(error));
+
+    return function cleanup() {
+      abortController.abort();
+    };
+  }, []);
+
+  if (orderHistoryData.length === 0) {
+    return (<Typography variant="h5" color="textSecondary">Items data unavailable.</Typography>);
+  } else {
+    return (
+      <Container>
+      </Container>
+    );
+  }
 }
