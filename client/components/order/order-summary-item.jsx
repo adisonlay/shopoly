21fd96eae3a10f71a9b4feb19d43e55d018c2749
Adison Lay <adisonlay@gmail.com>
2019-12-03 06:46:20 -0800

    Pass ordered item data to OrderSummaryItem component via props

--- client/components/order/order-summary-item.jsx
+++ client/components/order/order-summary-item.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import { formatItemData } from '../app/functions';
 import { Box, Paper, Grid, Typography, Link } from '@material-ui/core';
 
-export default function OrderSummaryItem() {
+export default function OrderSummaryItem({ itemData, setAppView }) {
+  const formattedData = formatItemData(itemData);
+  const {
+    cartID,
+    finalPrice,
+    quantity,
+    itemID,
+    name,
+    price,
+    lotNumber,
+    rent,
+    itemGroup,
+    images
+  } = formattedData;
+
+  const handleItemClick = () => setAppView('details', { itemID, itemName: name });
+
   return (
     <div>
 
