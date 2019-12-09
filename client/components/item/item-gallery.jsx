f11cdf8b579c593d11a151ac30cc521a663b54b8
Adison Lay <adisonlay@gmail.com>
2019-12-08 23:00:41 -0800

    Added basic render for ItemGallery component

--- client/components/item/item-gallery.jsx
+++ client/components/item/item-gallery.jsx
@@ -4,7 +4,17 @@ import { Box, Link } from '@material-ui/core';
 export default function ItemGallery({ images }) {
   return (
     <div>
-
+      <Box
+        mb="1rem"
+        width={1}
+        height="16rem"
+        style={{
+          backgroundImage: `url("${images[1]}")`,
+          backgroundSize: 'contain',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat'
+        }}
+      />
     </div>
   );
 }
