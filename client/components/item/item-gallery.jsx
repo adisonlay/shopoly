ff9bae21d437ac22e6f6dc9edb40278fdd425748
Adison Lay <adisonlay@gmail.com>
2019-12-09 05:01:21 -0800

    Added state hook to ItemGallery component for current slide

--- client/components/item/item-gallery.jsx
+++ client/components/item/item-gallery.jsx
@@ -1,20 +1,46 @@
-import React from 'react';
-import { Box, Link } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Grid, Box, Link } from '@material-ui/core';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export default function ItemGallery({ images }) {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   return (
-    <div>
-      <Box
-        mb="1rem"
-        width={1}
-        height="16rem"
-        style={{
-          backgroundImage: `url("${images[1]}")`,
-          backgroundSize: 'contain',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat'
-        }}
-      />
-    </div>
+    <Grid container>
+      <Grid item xs={3} direction="column">
+        {images.map(url => (
+          <Box
+            key={url + 'thumbs'}
+            width={1}
+            height="6rem"
+            style={{
+              backgroundImage: `url("${url}")`,
+              backgroundSize: 'contain',
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat'
+            }}
+          />
+        ))}
+      </Grid>
+
+      <Grid item xs={9}>
+        {images.map(url => (
+          <Box
+            key={url + 'slides'}
+            width={1}
+            height="16rem"
+            style={{
+              backgroundImage: `url("${url}")`,
+              backgroundSize: 'contain',
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat'
+            }}
+          />
+        ))}
+        <Link onClick={}><NavigateBeforeIcon /></Link>
+        <Link onClick={}><NavigateNextIcon /></Link>
+      </Grid>
+    </Grid>
   );
 }
