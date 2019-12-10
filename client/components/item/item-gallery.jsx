32d91c62a8ceaa36ec430ad2bb2d2e19fd8d45a2
Adison Lay <adisonlay@gmail.com>
2019-12-09 22:47:14 -0800

    Refactored render for ItemGallery component, fixed spacing, removed necessary elements and classes

--- client/components/item/item-gallery.jsx
+++ client/components/item/item-gallery.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Grid, Box, IconButton } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Box, Grid, IconButton } from '@material-ui/core';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
@@ -12,51 +12,36 @@ export default function ItemGallery({ images }) {
   const handleThumbnailClick = index => setCurrentSlide(index);
 
   return (
-    <Grid container>
-      <Grid item xs={3} container direction="column">
-        {images.map((url, index) => (
-          <Box
-            key={url + 'thumbs'}
-            my={slideCount > 3 ? '0.25rem' : '0.333rem'}
-            width={1}
-            height={slideCount > 3 ? '4.5rem' : '6rem'}
-            className={'gallery-thumbnails cursor ' + (index === currentSlide ? 'gallery-thumb-active' : '')}
-            style={{
-              backgroundImage: `url("${url}")`,
-              backgroundSize: 'contain',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat'
-            }}
-            onClick={() => handleThumbnailClick(index)}
-          />
-        ))}
-      </Grid>
-
-      <Grid item xs={9}>
-        <Box display="flex" alignItems="center">
-          <IconButton  onClick={handlePrev}>
-            <NavigateBeforeIcon />
-          </IconButton>
+    <Box mb="1rem">
+      <Grid container>
+        <Grid item xs={3} container direction="column" justify="space-between">
           {images.map((url, index) => (
             <Box
-              key={url + 'slides'}
-              mb="1rem"
+              key={url}
               width={1}
-              height="20rem"
-              className={'gallery-slides ' + (index === currentSlide ? 'gallery-slide-active' : '')}
-              style={{
-                backgroundImage: `url("${url}")`,
-                backgroundSize: 'contain',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat'
-              }}
+              height={slideCount > 3 ? '20%' : '30%'}
+              className={'gallery-thumbnails image-box cursor ' + (index === currentSlide ? 'gallery-thumb-active' : '')}
+              style={{ backgroundImage: `url("${url}")` }}
+              onClick={() => handleThumbnailClick(index)}
             />
           ))}
-          <IconButton  onClick={handleNext}>
+        </Grid>
+
+        <Grid item xs={9} container alignItems="center" wrap="nowrap">
+          <IconButton onClick={handlePrev}>
+            <NavigateBeforeIcon />
+          </IconButton>
+          <Box
+            width={1}
+            height="20rem"
+            className="image-box"
+            style={{ backgroundImage: `url("${images[currentSlide]}")` }}
+          />
+          <IconButton onClick={handleNext}>
             <NavigateNextIcon />
           </IconButton>
-        </Box>
+        </Grid>
       </Grid>
-    </Grid>
+    </Box>
   );
 }
