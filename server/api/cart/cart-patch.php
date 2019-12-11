9be5cdbacc4f4feee5949298454663e8b2d8cd27
Adison Lay <adisonlay@gmail.com>
2019-12-11 13:56:27 -0800

    Check for body data in cart-patch.php

--- server/api/cart/cart-patch.php
+++ server/api/cart/cart-patch.php
@@ -4,6 +4,16 @@ if (!defined('INTERNAL')) {
   exit('Direct access unavailable');
 }
 
+$bodyData = getBodyData();
 
+if (empty($bodyData['cartID']) || empty($_SESSION['activeCartID'])) {
+  throw new Exception('Cart ID missing; item not removed from cart');
+  exit();
+}
+
+if (empty($bodyData['itemID'])) {
+  throw new Exception('Item ID missing; item not removed from cart');
+  exit();
+}
 
 ?>
