66e6c9ef933f5b93aed2398233e64960a3036047
Adison Lay <adisonlay@gmail.com>
2019-12-10 22:59:25 -0800

    Added popper for free parking click in OrderHistoryItem component

--- client/components/order/order-history-item.jsx
+++ client/components/order/order-history-item.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { formatItemData } from '../app/functions';
-import { Box, Divider, Grid, Typography, Link } from '@material-ui/core';
+import { Box, Divider, Grid, Typography, Link, Popper } from '@material-ui/core';
 
 export default function OrderHistoryItem({ itemData, setAppView }) {
+  const [popperAnchor, setPopperAnchor] = useState(null);
+
   const formattedData = formatItemData(itemData);
   const {
     cartID,
@@ -20,7 +22,13 @@ export default function OrderHistoryItem({ itemData, setAppView }) {
 
   const orderDate = new Date(ordered);
 
-  const handleItemClick = () => setAppView('details', { itemID, itemName: name });
+  const handleItemClick = event => {
+    if (name === 'Free Parking') {
+      setPopperAnchor(popperAnchor ? null : event.currentTarget)
+      return;
+    }
+    setAppView('details', { itemID, itemName: name });
+  };
 
   return (
     <>
@@ -61,6 +69,10 @@ export default function OrderHistoryItem({ itemData, setAppView }) {
           <Typography variant="body2" color="textSecondary">Subtotal: ${finalPrice * quantity}</Typography>
         </Grid>
       </Grid>
+
+      <Popper open={!!popperAnchor} anchorEl={popperAnchor}>
+        <img src="assets/images/helper/bill.png" alt="$500 Bill" style={{ height: '8rem' }} />
+      </Popper>
     </>
   );
 }
