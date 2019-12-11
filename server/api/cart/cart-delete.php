d88ff1f407c845366c0bac5950b06a4d35c9cf8e
Adison Lay <adisonlay@gmail.com>
2019-12-11 12:42:23 -0800

    Added error handlers to cart-delete.php

--- server/api/cart/cart-delete.php
+++ server/api/cart/cart-delete.php
@@ -4,6 +4,37 @@ if (!defined('INTERNAL')) {
   exit('Direct access unavailable');
 }
 
+if (empty($_SESSION['activeCartID'])) {
+  throw new Exception('Cart ID missing; item not removed from cart');
+  exit();
+}
+
 $bodyData = getBodyData();
+
+if (empty($bodyData['itemID'])) {
+  throw new Exception('Item ID missing; item not removed from cart');
+  exit();
+}
+
+$itemID = intval($bodyData['itemID']);
+
+if ($itemID <= 0) {
+  throw new Exception('Invalid Item ID: ' . $bodyData['itemID']);
+  exit();
+}
+
+$selectQuery = "SELECT * FROM `items` WHERE `id` = {$itemID};";
+
+$selectResult = mysqli_query($conn, $selectQuery);
+
+if (!$selectResult) {
+  throw new Exception('Query error; invalid SELECT: ' . mysqli_error($conn));
+  exit();
+}
+
+if (mysqli_num_rows($selectResult) === 0) {
+  throw new Exception('Item ID: ' . $itemID . ' not found; item not removed from cart');
+  exit();
+}
 
 ?>
