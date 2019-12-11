2a5e7ab5015372b3211f18a4d2c82ba34f6032f5
Adison Lay <adisonlay@gmail.com>
2019-12-11 14:00:12 -0800

    Check body data is valid for cart-patch endpoint

--- server/api/cart/cart-patch.php
+++ server/api/cart/cart-patch.php
@@ -7,12 +7,36 @@ if (!defined('INTERNAL')) {
 $bodyData = getBodyData();
 
 if (empty($bodyData['cartID']) || empty($_SESSION['activeCartID'])) {
-  throw new Exception('Cart ID missing; item not removed from cart');
+  throw new Exception('Cart ID missing; cart item quantity not updated');
   exit();
 }
 
 if (empty($bodyData['itemID'])) {
-  throw new Exception('Item ID missing; item not removed from cart');
+  throw new Exception('Item ID missing; cart item quantity not updated');
+  exit();
+}
+
+if (empty($bodyData['newQuantity'])) {
+  throw new Exception('Item quantity missing; cart item quantity not updated');
+  exit();
+}
+
+$cartID = intval($bodyData['cartID']);
+$itemID = intval($bodyData['itemID']);
+$newQuantity = intval($bodyData['newQuantity']);
+
+if ($cartID <= 0 || $cartID !== intval($_SESSION['activeCartID'])) {
+  throw new Exception('Invalid Cart ID: ' . $bodyData['cartID']);
+  exit();
+}
+
+if ($itemID <= 0) {
+  throw new Exception('Invalid Item ID: ' . $bodyData['itemID']);
+  exit();
+}
+
+if ($newQuantity <= 0) {
+  throw new Exception('Invalid Quantity: ' . $bodyData['newQuantity']);
   exit();
 }
 
