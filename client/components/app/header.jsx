f0763ab992844d1a092da21324a841106b9e15e0
Adison Lay <adisonlay@gmail.com>
2019-12-16 17:37:45 -0800

    Made Header component responsive, each box element stacks vertically on small screens

--- client/components/app/header.jsx
+++ client/components/app/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, AppBar, Toolbar, Typography, IconButton, Tooltip, Badge } from '@material-ui/core';
+import { Box, AppBar, Toolbar, Typography, Tooltip, IconButton, Badge } from '@material-ui/core';
 import LocalMallTwoToneIcon from '@material-ui/icons/LocalMallTwoTone';
 import ShoppingCartTwoToneIcon from '@material-ui/icons/ShoppingCartTwoTone';
 
@@ -12,37 +12,42 @@ export default function Header({ setAppView, cartItemCount }) {
     <Box mb="1rem">
       <AppBar position="static">
         <Toolbar>
-          <Box className="cursor" p="0.25rem" my="0.5rem" bgcolor="error.main" borderRadius="1rem" onClick={handleLogoClick}>
-            <Box
-              pl="1.25rem"
-              pr="1rem"
-              fontWeight={700}
-              fontSize="h4.fontSize"
-              fontFamily="'KabaleMedium', 'KabobLight', 'Roboto', 'Helvetica', 'Arial', sans-serif"
-              letterSpacing="0.25rem"
-              border={3}
-              borderRadius="1rem"
-            >
-              SHOPOLY
-            </Box>
-          </Box>
-          <Box alignSelf="flex-end" ml="0.75rem" mb="0.25rem">
-            <Typography variant="subtitle1">The Property Trading Shop</Typography>
-          </Box>
+          <Box width={1} display="flex" flexWrap="wrap" alignItems="center">
 
-          <Box ml="auto">
-            <Tooltip title="Orders">
-              <IconButton onClick={handleOrdersClick}>
-                <LocalMallTwoToneIcon />
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Cart">
-              <IconButton onClick={handleCartClick}>
-                <Badge color="error" badgeContent={cartItemCount}>
-                  <ShoppingCartTwoToneIcon />
-                </Badge>
-              </IconButton>
-            </Tooltip>
+            <Box className="cursor" p="0.25rem" my="0.25rem" bgcolor="error.main" borderRadius="1rem" onClick={handleLogoClick}>
+              <Box
+                pl="1.25rem"
+                pr="1rem"
+                fontWeight={700}
+                fontSize="h4.fontSize"
+                fontFamily="'KabaleMedium', 'KabobLight', 'Roboto', 'Helvetica', 'Arial', sans-serif"
+                letterSpacing="0.25rem"
+                border={3}
+                borderRadius="1rem"
+              >
+                SHOPOLY
+              </Box>
+            </Box>
+
+            <Box alignSelf="flex-end" ml="0.5rem" mb="0.25rem">
+              <Typography variant="subtitle1">The Property Trading Shop</Typography>
+            </Box>
+
+            <Box ml="auto">
+              <Tooltip title="Orders">
+                <IconButton onClick={handleOrdersClick}>
+                  <LocalMallTwoToneIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Cart">
+                <IconButton onClick={handleCartClick}>
+                  <Badge color="error" badgeContent={cartItemCount}>
+                    <ShoppingCartTwoToneIcon />
+                  </Badge>
+                </IconButton>
+              </Tooltip>
+            </Box>
+
           </Box>
         </Toolbar>
       </AppBar>
